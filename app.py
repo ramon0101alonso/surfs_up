9f34ec996a15d1c9fb5eab40c988ada39b02d373
Ramon Alonso <ramonalonso@Ramons-MacBook-Pro.local>
2022-08-30 07:06:15 -0400

    module 9

--- app.py
+++ app.py
@@ -8,12 +8,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello world'
-if __name__ == "__main__":
-  app.run()
+
 
 
 # 4. Define the about route
-
+export FLASK_APP=app.py
 
 # 5. Define the contact route
 
